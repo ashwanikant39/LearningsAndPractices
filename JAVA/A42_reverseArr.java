a4cb60f459c57af2280d19e4d4fc84cf4e3580dc
Ashwani kant <ashwani.kant39@gmail.com>
2023-04-20 12:21:23 +0530

    20/04/2023

--- JAVA/A42_reverseArr.java
+++ JAVA/A42_reverseArr.java
@@ -1,21 +1,21 @@
 public class A42_reverseArr {
     public static void reverseA(int number[]) {
-        int start =0, end=number.length-1;
-         while(start<end){
-            int tem=number[start];
-            number[start]=number[end];
-            number[end]=tem;
+        int start = 0, end = number.length - 1;
+        while (start < end) {
+            int tem = number[start];
+            number[start] = number[end];
+            number[end] = tem;
             start++;
             end--;
-         }
+        }
 
     }
 
     public static void main(String[] args) {
-        int arr[]={100,2,25,4,5};
+        int arr[] = { 100, 2, 25, 4, 5 };
         reverseA(arr);
-        for(int i=0; i<=arr.length-1; i++){
-            System.out.print(arr[i]+" ");
+        for (int i = 0; i <= arr.length - 1; i++) {
+            System.out.print(arr[i] + " ");
         }
 
     }
